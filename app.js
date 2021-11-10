7d1732ebe6f8754ca03ad565ca32ab2385e3e33b
Michael Spagnolo <Mspagnolo27@gmail.com>
2021-11-10 12:55:50 -0500

    prepared for push to github

--- app.js
+++ app.js
@@ -38,7 +38,11 @@ const Area = mongoose.model('Area', areaSchema);
 
 
 //DB Connection'
-const dbUrl = "mongodb+srv://mspagnolo-admin:orange11@cluster0.lxizv.mongodb.net/LearningLog?retryWrites=true&w=majority"
+const password = process.env.PASSWORD 
+if (password==null||password==""){
+    password = ""
+}
+const dbUrl = "mongodb+srv://mspagnolo-admin:"+password+"@cluster0.lxizv.mongodb.net/LearningLog?retryWrites=true&w=majority"
 mongoose.connect(dbUrl);
 
 
@@ -202,7 +206,11 @@ app.post("/delete/:note",function (req,res) {
    res.redirect(/area/+area+"/focus/"+focus_+"/skill/"+skill)
 });
 //Run app
+let port = process.env.PORT;
+if(port==null||pert==""){
+    port = 3000;
+}
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log("Server started on port 3000")
 });
